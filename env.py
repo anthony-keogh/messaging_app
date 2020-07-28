866b64bed4b791858afafd5391300b795d0c83e5
anthony keogh <siliconirish@gmal.com>
2020-07-28 21:36:17 +0100

    Commit message

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 os.environ.setdefault("MONGO_DBNAME", 'CookbookFask')
 os.environ.setdefault("MONGO_URI", 'mongodb://AnthonyKeogh:45hungryhill@cookbookfask-shard-00-00-t3cez.mongodb.net:27017,cookbookfask-shard-00-01-t3cez.mongodb.net:27017,cookbookfask-shard-00-02-t3cez.mongodb.net:27017/test?ssl=true&replicaSet=CookbookFask-shard-0&authSource=admin&retryWrites=true&w=majority')
+#mongodb://heroku_4kqdvlps:dua35col70q0kd730njouaph8l@ds055762.mlab.com:55762/heroku_4kqdvlps
