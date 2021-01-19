d37747dbbc0945fe3e800c823fdd58e392d355b2
anthony keogh <siliconirish@gmal.com>
2021-01-19 12:12:36 +0000

    Config changes

--- app/config.py
+++ app/config.py
@@ -6,23 +6,24 @@ import config
 class Config(object):
     DEBUG = False
     TESTING = False
-    MONGO_DBNAME = 'CookbookFask'
-    MONGO_URI = os.environ.setdefault("MONGO_URI", 'mongodb://AnthonyKeogh:45hungryhill@cookbookfask-shard-00-00-t3cez.mongodb.net:27017,cookbookfask-shard-00-01-t3cez.mongodb.net:27017,cookbookfask-shard-00-02-t3cez.mongodb.net:27017/test?ssl=true&replicaSet=CookbookFask-shard-0&authSource=admin&retryWrites=true&w=majority')
+    app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
     SECRET_KEY = os.getenv("SECRET_KEY")
     
-    DATABASE_URI = os.environ.setdefault("MONGO_URI", 'mongodb://AnthonyKeogh:45hungryhill@cookbookfask-shard-00-00-t3cez.mongodb.net:27017,cookbookfask-shard-00-01-t3cez.mongodb.net:27017,cookbookfask-shard-00-02-t3cez.mongodb.net:27017/test?ssl=true&replicaSet=CookbookFask-shard-0&authSource=admin&retryWrites=true&w=majority')
-
+    DATABASE_URI = os.environ.get('MONGO_URI')
 class ProductionConfig(Config):
     DEBUG = False
     
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    DATABASE_URI = os.environ.setdefault("MONGO_URI", 'mongodb://AnthonyKeogh:45hungryhill@cookbookfask-shard-00-00-t3cez.mongodb.net:27017,cookbookfask-shard-00-01-t3cez.mongodb.net:27017,cookbookfask-shard-00-02-t3cez.mongodb.net:27017/test?ssl=true&replicaSet=CookbookFask-shard-0&authSource=admin&retryWrites=true&w=majority')
-    TESTING = True
-    MONGO_DBNAME = 'CookbookFask'
-    MONGO_URI = os.environ.setdefault("MONGO_URI", 'mongodb://AnthonyKeogh:45hungryhill@cookbookfask-shard-00-00-t3cez.mongodb.net:27017,cookbookfask-shard-00-01-t3cez.mongodb.net:27017,cookbookfask-shard-00-02-t3cez.mongodb.net:27017/test?ssl=true&replicaSet=CookbookFask-shard-0&authSource=admin&retryWrites=true&w=majority')
+    app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
     SECRET_KEY = os.getenv("SECRET_KEY")
+    
+    DATABASE_URI = os.environ.get('MONGO_URI')
+    TESTING = True
+   
     #app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 class TestingConfig(Config):
